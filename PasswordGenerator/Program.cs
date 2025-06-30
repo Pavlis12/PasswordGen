306c64dc03c7982b00fcb128bd6043719a03b5d5
pavel svoboda <pavel97svoboda@seznam.cz>
2025-06-30 14:34:58 +0200

    basci setup

---
+++ PasswordGenerator/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using 
+
+namespace PasswordGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int vyber = rnd.Next(1, 3);
+            for (int i = 0; i < 8; i++)
+            {
+                if (vyber == 1)
+                {
+                    SpecZnak();
+                }
+                if (vyber == 2)
+                {
+                    Cislo();
+                }
+                if (vyber == 3)
+                {
+                    Pismeno();
+                }
+            }
+        }
+
+        private static void Pismeno()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void Cislo()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void SpecZnak()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
